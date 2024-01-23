a20b54e34a484da406745d66968a6187158fc297
Cleo Alcanzo <94949037+Azerux123@users.noreply.github.com>
2024-01-23 15:33:07 +0800

    update everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+// Import all the components here----->
+import Sidebar from './Components/Sidebar Section/Sidebar'
+import Body from './Components/Body Section/Body'
+import Account from './Components/Account Section/Account'
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      
+      <Sidebar/>
+      <Body/>
+      <Account/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
